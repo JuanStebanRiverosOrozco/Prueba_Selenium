d96d717bc9379a0d185849ad7fd8414981ab1b8f
juan <juanstebanriveros@gmail.com>
2026-08-19 18:06:57 -0500

    Acabado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args){
@@ -27,6 +32,46 @@ public class Main {
         else{
             System.out.println("La prueba fue fallida");
         }*/
+        /*//apertura del navegador
+        WebDriver driver = new ChromeDriver();
+        //generar  la espera explicita
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        try {
+            driver.get("https://the-internet.herokuapp.com/inputs");
+            WebElement elemento = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("input")));
+            elemento.sendKeys("15");
+        }finally {
+            driver.quit();
+        }*/
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+
+        try {
+            driver.get("https://www.wikipedia.org/");
+            driver.manage().window().maximize();
+
+            WebElement inputBusqueda = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("search")));
+
+            inputBusqueda.sendKeys("selenium");
+            inputBusqueda.sendKeys(Keys.ENTER);
+
+            String encabezado = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstHeading"))).getText();
+
+            System.out.println("El encabezado es: "+ encabezado);
+
+            if(encabezado.toLowerCase().contains("selenium")) {
+                System.out.println("Paso la prueba");
+            }else{
+                System.out.println("No paso la prueba");
+            }
+            
+        }finally {
+            driver.quit();
+        }
+
+
     }
 
 }
